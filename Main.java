a7925996ad441b73d9bd43c3ce9339a5d8b08e4f
FlevieLans <flownart@gmail.com>
2024-05-24 22:13:40 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -4,34 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws CalculatorException {
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        calc(input);
+    }
+
+    public static void calc(String input) throws CalculatorException {
+
         String[] inputParts = input.split(" ");
 
         if (inputParts.length > 3) { throw new CalculatorException("Формат математической операции не удовлетворяет заданию."); }
         if (inputParts.length < 3) { throw new CalculatorException("Строка не является математической операцией."); }
 
         String[][] numbers = {{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                               "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-                               "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
-                               "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
-                               "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
-                               "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
-                               "61", "62", "63", "64", "65", "66", "67", "68", "69", "70",
-                               "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
-                               "81", "82", "83", "84", "85", "86", "87", "88", "89", "90",
-                               "91", "92", "93", "94", "95", "96", "97", "98", "99", "100" },
-                                    { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
-                                      "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                                      "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
-                                      "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
-                                      "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
-                                      "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
-                                      "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
-                                      "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-                                      "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                                      "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C" }};
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+                "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+                "61", "62", "63", "64", "65", "66", "67", "68", "69", "70",
+                "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
+                "81", "82", "83", "84", "85", "86", "87", "88", "89", "90",
+                "91", "92", "93", "94", "95", "96", "97", "98", "99", "100" },
+                { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                        "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                        "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+                        "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                        "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+                        "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                        "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                        "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                        "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                        "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C" }};
 
         int resultSwitcher1 = 0;
         int resultSwitcher2 = 0;
@@ -128,7 +132,7 @@ public class Main {
         int i = 0;
 
         if (a > 10 || b > 10) { throw new CalculatorException("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более."); }
-        if (resultSwitcher1 == 1) { throw new CalculatorException("Калькулятор умеет работать только с арабскими или римскими цифрами одновременно."); }
+        if (resultSwitcher1 + resultSwitcher2 == 1) { throw new CalculatorException("Калькулятор умеет работать только с арабскими или римскими цифрами одновременно."); }
 
         switch (inputParts[1]) {
             case "+":
